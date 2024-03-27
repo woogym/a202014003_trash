a90259dcd29948ddd3c89085552baa0d703b22a3
woogym <kwj9294@gmail.com>
2024-03-27 12:22:37 +0900

    third

--- serverbuild_0327/src/Main.java
+++ serverbuild_0327/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("홍길동");
+        System.out.println("김우진");
+        System.out.println("하나");
     }
 }
